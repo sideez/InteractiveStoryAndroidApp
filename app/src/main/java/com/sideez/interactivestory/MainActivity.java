4df26ccfc9208c0344e9816467e3912861c2da25
Faisal Ali <siddee.faisalali@gmail.com>
2015-06-25 00:32:18 -0400

    create StoryActivity and added Intent

--- app/src/main/java/com/sideez/interactivestory/MainActivity.java
+++ app/src/main/java/com/sideez/interactivestory/MainActivity.java
@@ -1,17 +1,16 @@
 package com.sideez.interactivestory;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText mNameField;
-    private Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mNameField = (EditText) findViewById(R.id.nameEditText);
-        mStartButton = (Button) findViewById(R.id.startButton);
+        Button startButton = (Button) findViewById(R.id.startButton);
 
-        mStartButton.setOnClickListener(new View.OnClickListener() {
+        startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String name = mNameField.getText().toString();
-                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+                startStory(name);
             }
         });
+    }
+
+    private void startStory(String name) {
+        Intent intent = new Intent(this, StoryActivity.class);
+        intent.putExtra(getString(R.string.key_name), name);
+        startActivity(intent);
     }
 
 }
---
+++ app/src/main/java/com/sideez/interactivestory/StoryActivity.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2015.
+ */
+
+package com.sideez.interactivestory;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+
+public class StoryActivity extends AppCompatActivity {
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(getString(R.string.key_name));
+        if (name == null) {
+            name = "Friend";
+        }
+        Log.d(TAG, name);
+    }
+
+}
